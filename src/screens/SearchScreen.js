26f906960ae81198e5ab89de9ce71621546cfe23
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2020-07-18 01:54:25 +0545

    Initial Commit - Navigation + all required dependencies

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, Button, StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <Text>
+            SearchScreen.
+        </Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
